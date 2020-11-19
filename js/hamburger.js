be9cfe6a0c7072c3ccd44c04f1e63840ef59b054
Yaroslav-Bondar <72633393+Yaroslav-Bondar@users.noreply.github.com>
2020-11-19 07:56:18 +0200

    Add files via upload
    breguet v_1.1.1

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,12 @@
 const hamburger = document.querySelector(".main-scren__line-nav-block");
 const navLinks = document.querySelector(".main-screen__links-nav");
-const bodyTag = document.getElementsByTagName("body");
+// const bodyTag = document.getElementsByTagName("body");
+const htmlTag = document.querySelector("html");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("change");
   navLinks.classList.toggle("open");
   // bodyTag[0].style.overflow = "hidden";
-  bodyTag[0].classList.toggle("lock");
+  // bodyTag[0].classList.toggle("lock");
+  htmlTag.classList.toggle("lock");
 });
--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,32 @@
 var slideIndex = 0;
+var slides = document.querySelectorAll(".main-screen__slides.ibg");
+var dots = document.querySelectorAll(".main-screen__dot");
 showSlides();
-
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("main-screen__slides");
-  var dots = document.getElementsByClassName("main-screen__dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" main-screen__dot-active", "");
+  if (slideIndex > slides.length) {
+      slideIndex = 1
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " main-screen__dot-active";
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("main-screen__dot-active");
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("main-screen__slides_active");
+  }
+  slides[slideIndex-1].classList.add("main-screen__slides_active");
+  dots[slideIndex-1].classList.add("main-screen__dot-active");
   setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
+dots.forEach((indicator, i) => {
+  indicator.addEventListener('click', () => {
+    dots.forEach((item) => {
+      item.classList.remove("main-screen__dot-active");
+    });
+    slides.forEach((item) => {
+      item.classList.remove("main-screen__slides_active");
+    });
+    indicator.classList.add('main-screen__dot-active');
+    slides[i].classList.add("main-screen__slides_active");
+    slideIndex = i;
+  });
+});
